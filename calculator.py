d8725497c84d7e4e9413baf8b5f6015e1e556acf
Alison Fang <afan0409@uni.sydney.edu.au>
2026-01-18 13:31:14 +1100

    hi :)

--- calculator.py
+++ calculator.py
@@ -1,2 +1,11 @@
 def add_two(x):
     return x + 2
+
+def wam(list_of_grades):
+    sum = 0
+    for grade in list_of_grades:
+        sum = sum + grade
+        
+    average = sum/len(list_of_grades)
+    return average
+
--- cli.py
+++ cli.py
@@ -1,3 +1,6 @@
 import calculator
 
-print(calculator.add_two(5))
+# print(calculator.add_two(5))
+
+print(calculator.wam([99, 3, 27, 89]))
+      
